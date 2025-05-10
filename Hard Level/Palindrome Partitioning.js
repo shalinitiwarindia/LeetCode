f1504a2248288014a7ae22cb5aab7ce05150520c
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-05-10 21:16:28 +0530

    Update Palindrome Partitioning.js

--- Hard Level/Palindrome Partitioning.js
+++ Hard Level/Palindrome Partitioning.js
@@ -4,8 +4,7 @@
  */
 var minCut = function(s) {
     const n = s.length;
-    const cuts = new Array(n).fill(0);
-    const isPalindrome = Array.from({ length: n }, () => new Array(n).fill(false));
+ 
     
    
     
