3aa63a29b6d602134412d44ba2a8e1023bae0f66
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-05-15 20:23:17 +0530

    Update Longest Unequal Adjacent Groups Subsequence I.js

--- JS-Easy Level Sum/Longest Unequal Adjacent Groups Subsequence I.js
+++ JS-Easy Level Sum/Longest Unequal Adjacent Groups Subsequence I.js
@@ -1,1 +1,12 @@
+var getLongestSubsequence = function(words, groups) {
+    const result = [];
+    let last = -1;
+    for (let i = 0; i < words.length; i++) {
+        if (groups[i] !== last) {
+            result.push(words[i]);
+            last = groups[i];
+        }
+    }
+    return result;
+};
 
