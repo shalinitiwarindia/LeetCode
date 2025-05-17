cef3a625f5e1fb8d8718b2014073bc6ef0547b0a
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-05-17 20:11:32 +0530

    Create Sort Colors.js

---
+++ Medium Level Sums/Sort Colors.js
@@ -0,0 +1,22 @@
+var sortColors = function(nums) {
+    let zero = 0, one = 0, two = 0;
+
+    for (let n of nums) {
+        if (n === 0) zero++;
+        else if (n === 1) one++;
+        else two++;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (zero > 0) {
+            nums[i] = 0;
+            zero--;
+        } else if (one > 0) {
+            nums[i] = 1;
+            one--;
+        } else {
+            nums[i] = 2;
+            two--;
+        }
+    }
+};
