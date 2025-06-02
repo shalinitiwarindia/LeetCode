2ae73487b31d489e431eb1f6c845c9ab441881f4
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-06-02 22:47:00 +0530

    Update Maximize the Number of Target Nodes After Connecting Trees II.js

--- Hard Level/Maximize the Number of Target Nodes After Connecting Trees II.js
+++ Hard Level/Maximize the Number of Target Nodes After Connecting Trees II.js
@@ -12,12 +12,7 @@ var maxTargetNodes = function(edges1, edges2) {
             res += dfs(w, v, adj, parity, !even);
         }
      
-    let n1 = edges1.length+1, n2 = edges2.length+1;
-    console.log(n1, n2);
-    let parity1 = Array.from({length:n1}, () => false);
-    let parity2 = Array.from({length:n2}, () => false);
-    let even1 = dfs(0, -1, adj(edges1, n1), parity1, true), odd1 = n1 - even1;
-    let even2 = dfs(0, -1, adj(edges2, n2), parity2, true), odd2 = n2 - even2;
+    le
     let res = [];
     for(let i = 0; i < n1; ++i){
         res.push((parity1[i]?even1:odd1) + Math.max(even2,odd2));
