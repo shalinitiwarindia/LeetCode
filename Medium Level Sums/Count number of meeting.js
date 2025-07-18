4ca7b3ba4692c4448e7e3bb536bffe8a48a01efc
Shalini Tiwari <130977588+shalinitiwarindia@users.noreply.github.com>
2025-07-18 20:54:38 +0530

    Update Count number of meeting.js

--- Medium Level Sums/Count number of meeting.js
+++ Medium Level Sums/Count number of meeting.js
@@ -3,11 +3,5 @@
 
    
 
-    // Step 3: Calculate available days
-    return days - mergedDays;
-};
-
-// Test cases
-console.log(countDays(10, [[5,7],[1,3],[9,10]])); // Output: 2
-console.log(countDays(5, [[2,4],[1,3]])); // Output: 1
+ 
 
